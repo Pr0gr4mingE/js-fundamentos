9a19f767c3616f5bbe7fe47ca4e4ca7ad73db6e4
Progr4ming3 <victoremiliano.purificacao@gmail.com>
2026-06-18 20:51:35 -0300

    feat: adiciona pratica de const e array
    cria lista de herois(name, class, city)

---
+++ fase01/pratica01.js
@@ -0,0 +1,12 @@
+const dcheros = [
+
+    {name: "Superman", class: "Super", city: "Metropolis"},
+    {name: "Batman", class: "Vigilante", city: "Gotham city"},
+    {name: "Green Arrow", class: "Vigilante", city: "Star city"},
+    {name: "Flash", class: "Super", city: "Central city"},
+    {name: "Green Lantern", class: "Super", city: "Coast city"}
+
+
+]
+
+console.log(dcheros)
