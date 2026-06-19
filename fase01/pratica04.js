95256d60a1d1f6dbdc5dadaa303d89983d170981
Progr4ming3 <victoremiliano.purificacao@gmail.com>
2026-06-18 23:29:02 -0300

    feat: adiciona pratica de switch com fall-trough
    cria logica de barra de especial com switch e ++ pra simulacao ate o break

---
+++ fase01/pratica04.js
@@ -0,0 +1,19 @@
+
+//valor inicial da barra de especial
+let barradeespecial = 0
+
+//controle de fluxo da barra de especial
+switch(barradeespecial) {
+
+case 0:
+    barradeespecial ++;
+
+case 1:
+        console.log('=====50%\nContinue lutando!!!');
+case 2:
+        console.log('=======75%\nPARTIU ULTAR!!!');
+case 3:
+        console.log('==========100%\nLEROOOOOOOOOOOOOY JEEEEEEEEEEEENNNNNNNNKINS!!!!!!!!!!!!! ');
+            break;
+}
+
