a5d58ce8c372f4a24966fbd43eaa697cc209b887
Progr4ming3 <victoremiliano.purificacao@gmail.com>
2026-06-18 22:20:17 -0300

    feat: adiciona pratica de atribuicao de valores const e let
    cria logica com valor de servico por hora(valor x hora) e reatribuicao de valor

---
+++ fase01/pratica03.js
@@ -0,0 +1,10 @@
+//valor por 1 hora
+let valor = 5
+//quantidade de horas
+let hora = 2
+//valor pela quantidade de horas
+const valorhora = (valor * hora)
+//novo valor
+valor = valorhora
+//imprime o enunciado com o valor a ser pago pelo cliente
+console.log("O valor total do serviço será R$",valor)
