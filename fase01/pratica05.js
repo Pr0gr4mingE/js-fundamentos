58df0d270bf96315824d04781594ece307f6b6db
Progr4ming3 <victoremiliano.purificacao@gmail.com>
2026-06-20 12:03:25 -0300

    feat: adiciona pratica de if e else
    cria condicionais para golpes da barra de especial com if e operadores logicos &&

---
+++ fase01/pratica05.js
@@ -0,0 +1,23 @@
+barradeespecial = 120
+
+if(barradeespecial >= 25 && barradeespecial < 50){
+
+    console.log('25%==\nCOMBO BREAKER LIBERADO!')
+
+}
+
+if(barradeespecial >= 50 && barradeespecial < 100){
+
+    console.log('50%====\nEXTREME COMBO LIBERADO!')
+
+}
+
+if(barradeespecial >= 100){
+
+    console.log('100%========\nULTRA COMBO MEGA BRAIN LIBERADO!')
+
+}
+
+else{
+console.log('FIGHT!')
+}
