d37bf3d72ab0d8927999b2768354692076fda37b
Progr4ming3 <victoremiliano.purificacao@gmail.com>
2026-06-20 23:09:32 -0300

    feat: adiciona pratica de laco for e operador ternario
    cria repeticao para tempo de estudo e usa ternario para corrigir o plural das horas

---
+++ fase01/pratica08.js
@@ -0,0 +1,7 @@
+const horasaestudar = 4
+
+for (let tempodeestudo = 0; tempodeestudo < horasaestudar; tempodeestudo++ ) {
+
+    console.log(`Tempo de estudo em ${tempodeestudo} ${tempodeestudo >= 2 ? 'horas' : 'hora' }`);
+
+}
