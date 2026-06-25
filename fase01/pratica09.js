654797e51ac1ebd1594018ec0b00baedd73f561b
Pr0gr4mingE <victoremiliano.purificacao@gmail.com>
2026-06-25 16:08:13 -0300

    feat: adiciona pratica de while
    cria simulacao de batalha por turnos com while pra compor os turnos de acao

---
+++ fase01/pratica09.js
@@ -0,0 +1,43 @@
+let superman = {class: "Super", power: 300}
+
+let lexluthor = {class: "supervillain", power: 600}
+
+
+
+
+console.log("Superman: LUTHOOOOOOOOOOR!!!\n")
+    console.log("Lex Luthor: BRAIN BEATS BRAWL\n")
+
+while (lexluthor.power > 0) {
+
+    console.log("Superman: SUUUUUUUPER PUNCH!!!\n")
+        let superpunch = ( lexluthor.power = (lexluthor.power - superman.power))
+            console.log(`Lex Luthor: ARRGHHHH!!!\n Lex luthor power: ${lexluthor.power}\n`)
+               if (lexluthor.power <= 0) {
+                    break;
+               }
+
+}
+
+
+if (lexluthor.power <= 0) {
+
+    const round02 = (lexluthor.power = 600)
+        let kriptonitearmour = (lexluthor.power = (lexluthor.power * 2))
+                console.log(`Lex Luthor: Ativar armadura de Kriptonita!\nLex Luthor power: ${lexluthor.power}\n`)
+}
+
+    let kriptoniteslash = ( superman.power = (superman.power - 75))
+        console.log("Lex Luthor: KRIIIIIIIIPTONITE SLAAAAAAAAAASH!!!\n")
+            console.log(`superman power: ${superman.power}`)
+
+while (lexluthor.power > 0) {
+
+    console.log("Superman: SUUUUUUUPER PUNCH!!!\n")
+        let superpunch = ( lexluthor.power = (lexluthor.power - superman.power))
+                superpunch
+                    console.log(`Lex Luthor: ARRGHHHH!!!\n Lex luthor power: ${lexluthor.power}`)
+                        if (lexluthor.power <= 0) {
+                            break;
+                    }
+}
