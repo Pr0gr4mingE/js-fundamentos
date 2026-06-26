71b183c59636fb1b974859efee7d0408f89bc690
Pr0gr4mingE <victoremiliano.purificacao@gmail.com>
2026-06-26 14:43:07 -0300

    feat: cria acumulador de producao
    usa laco for para simular horas de um turno e acumular producao de microchips

---
+++ fase01/pratica13.js
@@ -0,0 +1,9 @@
+let mircroshipsprocessados = 0
+
+for (let horas = 0; horas < 8; horas++ ) {
+
+    mircroshipsprocessados += 45
+
+}
+
+console.log(`microships montados durante o turno: ${mircroshipsprocessados}`)
