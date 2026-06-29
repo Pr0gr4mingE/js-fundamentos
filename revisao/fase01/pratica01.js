c5922c4d77f6acf10bc23aab66fb4cc21842cf10
Progr4ming3 <victoremiliano.purificacao@gmail.com>
2026-06-29 13:30:40 -0300

    feat: adiciona pratica de inventario basico
    cria array de objetos e variaveis para revisao de sintaxe e renderiza com console.table

---
+++ revisao/fase01/pratica01.js
@@ -0,0 +1,11 @@
+const gabinete = [
+
+    {nome: "Processador", preco: 989},
+    {nome: "Memória Ram", preco: 799},
+    {nome: "Placa Mãe", preco: 1759}
+
+]
+
+const orcamentomaximo = 2500
+
+console.table(gabinete)
